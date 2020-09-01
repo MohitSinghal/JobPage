92ebdaf42fc2258e79cddb409cb98765532dc327
MohitSinghal <mohit.singhal89@gmail.com>
2020-09-01 23:49:14 +0530

    MVVM Architecture instantiated, SplashViewController added.

---
+++ JobPage/JobPage/SplashViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SplashViewController.swift
+//  JobPage
+//
+//  Created by mohit on 01/09/20.
+//  Copyright © 2020 MSTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplashViewController: UIViewController {
+
+}
